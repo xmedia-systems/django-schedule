f0249729979c56babfbaaabf2c0eb180c5ab3cdb
Carlos Zúñiga <carlos.zun@gmail.com>
2009-11-22 12:37:25 -0500

    validarion for params field in the Rule model

--- schedule/forms.py
+++ schedule/forms.py
@@ -32,3 +32,21 @@ class OccurrenceForm(SpanForm):
     class Meta:
         model = Occurrence
         exclude = ('original_start', 'original_end', 'event', 'cancelled')
+
+
+class RuleForm(forms.ModelForm):
+    params = forms.CharField(widget=forms.Textarea, help_text=_("Python rrule.rrule parameters like bysetpos:1;byweekday=5"))
+
+    def clean_params(self):
+        params = self.cleaned_data["params"]
+        try:
+            params = params.split(';')
+            for param in params:
+                param = param.split(':')
+                if len(param) == 2:
+                    param = (str(param[0]), [int(p) for p in param[1].split(',')])
+                    if len(param[1]) == 1:
+                        param = (param[0], param[1][0])
+        except ValueError:
+            raise forms.ValidationError(_("Params format looks invalide"))
+        return self.cleaned_data["params"]
