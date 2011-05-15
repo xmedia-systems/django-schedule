6a5ed12ee0186ae6358ad6e56357cfd526396965
John Anderson <sontek@gmail.com>
2011-05-15 17:53:38 -0500

    updated to latest admin info

--- project_sample/urls.py
+++ project_sample/urls.py
@@ -9,12 +9,8 @@ urlpatterns = patterns('',
     url(r'^$', direct_to_template,{"template":"homepage.html"}),
     (r'^schedule/', include('schedule.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
